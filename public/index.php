73d65cde303217de12919dbee52b0616bbcee83b
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2025-06-09 20:22:21 +0200

    MAJ index

--- public/index.php
+++ public/index.php
@@ -1,26 +1,5 @@
 <?php
 
-include '../bootstrap.php';
+require_once 'controllers/noteController.php'
 
-$loc = filter_input(INPUT_GET, "loc");
-$action = filter_input(INPUT_POST, "action");
-var_dump($loc, $action);
-
-$controller = null;
-
-switch ($loc) {
-    case "tag":
-        include(BASE_PATH."src/controllers/TagController.php");
-        $controller = new TagController($actoin);
-        // $controller->index();
-        // $controller->show();
-
-        break;
-    case "note":
-        include("app/controllers/NoteController.php");
-        $controller = new NoteControleur($action);
-        break;
-}
-
-$controller->run();
-
+$controller = new NoteController("index");
