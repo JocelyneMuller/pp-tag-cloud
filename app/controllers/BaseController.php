bb600e7997725126c9d9a1d316176fa57512e678
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2025-06-09 20:13:18 +0200

    Edition code BaseController

---
+++ app/controllers/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+
+abstract class BaseController {
+    public function __construct($action) {
+        echo "Je suis dans le contrôleur " . get_class($this) . " action : " . $action;
+
+        switch ($action) {
+            case "index":
+                $this->index();
+                break;
+            case "show":
+                $this->show();
+                break;
+            default:
+                echo "404";
+                break;
+        }
+    }
+
+    // On déclare les méthodes attendues, mais on ne les définit pas ici
+    abstract public function index();
+    abstract public function show();
+}
