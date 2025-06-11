4ec2da09890ae54f71be8d2c49b5bc90cc9a9026
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2025-06-11 16:25:32 +0200

    TODO2.1

---
+++ app/widgets/src/components/MyButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button :class="['btn', variant]">{{ label }}</button>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: 'Mon bouton',
+  },
+  variant: {
+    type: String,
+    default: 'primary', // "primary" ou "secondary"
+  },
+})
+</script>
+
+<style scoped>
+.btn {
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+.primary {
+  background: royalblue;
+  color: white;
+}
+.secondary {
+  background: lightgray;
+  color: black;
+}
+</style>
